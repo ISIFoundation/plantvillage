0adb33ad750161ea184f36a1dc69fc5e63a680e2
André Panisson <panisson@gmail.com>
2016-05-26 20:15:42 +0200

    Initial model

---
+++ model/GoogLeNet/train.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CAFFE_HOME=/home/ubuntu/caffe
+PLANTVILLAGE_PATH=/home/ubuntu/PlantVillage/plantvillage
+LMDB_PATH=$PLANTVILLAGE_PATH/lmdb
+MODEL_PATH=$PLANTVILLAGE_PATH/model/GoogLeNet
+
+cd $PLANTVILLAGE_PATH
+
+$CAFFE_HOME/build/tools/caffe train -solver $MODEL_PATH/solver.prototxt
